667e6eb9debdcceba85c01ea542d9c0fcba8b9e7
Piffek <patrykpiwko123412@gmail.com>
2018-03-15 17:53:40 +0100

    improvlemt task 3 - add weight

--- app/src/main/java/com/example/patrykpiwko/warehouse_my/Task3Activity.java
+++ app/src/main/java/com/example/patrykpiwko/warehouse_my/Task3Activity.java
@@ -3,11 +3,11 @@ package com.example.patrykpiwko.warehouse_my;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Task1Activity extends AppCompatActivity {
+public class Task3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_task1);
+        setContentView(R.layout.activity_task3);
     }
 }
